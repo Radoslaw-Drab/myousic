a981a11a8825256f91cfc725f61d8af3108cc432
Radosław Drab <radoslaw.drab03@gmail.com>
2023-02-28 20:55:53 +0100

    added comments

--- myousic.js
+++ myousic.js
@@ -32,36 +32,52 @@ script()
 
 async function script() {
 	console.clear()
+	// Gets data from the clipboard
 	const clipboard = (await getCommands('pbpaste'))[0].value
 
+	// Gets data from the clipboard if `clipboard` property is set
 	const getFromClipboard = properties.clipboard && clipboard
-	const getFromSearch = properties.search?.replace(/\..*/g, '').toString()
+	// Gets data from `search` property
+	const getFromSearch = properties.search
+	// Boolean which determines if url was set
 	const getFromUrl = properties.url === true
+	// Gets URL based on `url` property. Gets value from clipboard if `url` does not contain any url afterwards
 	const url = getFromUrl ? clipboard : properties.url
 
+	// Returns song name based on YouTube title
 	const songName =
 		properties.url &&
 		(await getCommands(`yt-dlp --print "%(title)s" ${url}`))[0]?.value
-			?.replace(/(\(|\[).*(\)|\])/gi, '')
-			.replace(/\(*(ft|feat).*/gi, '')
+			// Replaces anything that is contained inside of [] or ()
+			?.replace(/\(.*\)/gi, '')
+			.replace(/\[.*\]/gi, '')
+			// Replaces any ' x ' to ', '
 			.replace(/ x /gi, ', ')
 
+	// Gets term to search based on hierarchy: url > clipboard > search > no properties
 	const term = songName || getFromClipboard || getFromSearch || (await question('What to search: '))
 
 	const attributes = {
 		term,
 		entity: 'song',
+		// Allows to change limit
 		limit: properties.limit?.replace(/\D/g) || DEFAULT_LIMIT
 	}
 
+	// Variable with every attiribute inside of `attributes` object formatted to math proper URI
 	const formattedAttributes = Object.keys(attributes).reduce((str, attribute, i) => {
+		// Gets encodedURI with current attribute
 		const encodedAtt = encodeURIComponent(attributes[attribute])
+		// Determines whether to add `&` and adds attribute to URI
 		const att = `${i > 0 ? '&' : ''}${attribute}=${encodedAtt}`
+		// Adds `att` to string only if `encodedAtt` is set
 		return encodedAtt ? (str += att) : str
 	}, '?')
 
+	// Fetches properly formatted URL
 	fetch(BASE_URL + formattedAttributes)
 		.then((response) => {
+			// Checks whether script failed to get response. If so prints error to the console and returns
 			if (!response.ok) {
 				console.clear()
 				const errorText = `ERROR\nCode: ${response.status}\nStatus: ${response.statusText}`
@@ -74,11 +90,15 @@ async function script() {
 		})
 		.then(async (data) => {
 			function sort(a, b) {
+				// Default searching from data received from iTunes
 				let searchType = 'trackName'
+				// All sort options
 				const sortOptions = ['asc', 'desc']
+				// Sets default sort option
 				let sort = sortOptions[0]
-
 				let prop = ''
+
+				// Tests if property is a string and sets `prop` to proper property and `searchType` to type
 				function setOptions(test, type) {
 					if (typeof properties[test] === 'string') {
 						prop = test
@@ -90,37 +110,54 @@ async function script() {
 				setOptions('sortArtist', 'artistName')
 				setOptions('sortTrack', 'trackName')
 
+				// Checks if property exists
 				if (typeof properties[prop] === 'string') {
+					// Sets sorting option if found or set's default option
 					sort = sortOptions.find((opt) => opt === properties[prop]) || sortOptions[0]
 				}
 
 				let sortDir = 1
+				// If sorting is set to `desc` then order will reverse
 				if (sort === 'desc') {
 					sortDir *= -1
 				}
 
+				// Compares strings
 				return a[searchType].localeCompare(b[searchType]) * sortDir
 			}
 			function filter(song) {
 				const { track, artist, album, year } = properties
 				const { trackName, artistName, collectionName, releaseDate } = song
+
+				// Checks whether `trackName` includes anything from `track` property
 				const sameTrack = track ? trackName.toLowerCase().includes(track.toLowerCase()) : true
+				// Checks whether `artistName` includes anything from `artist` property
 				const sameArtist = artist ? artistName.toLowerCase().includes(artist.toLowerCase()) : true
+				// Checks whether `collectionName` includes anything from `album` property
 				const sameAlbum = album ? collectionName.toLowerCase().includes(album.toLowerCase()) : true
+				// Checks whether `releaseDate` includes anything from `year` property
 				const sameYear = year ? (new Date(releaseDate).getFullYear().toString() || '').includes(year.toLowerCase()) : true
+				// Returns true only if every property is truthy
 				return sameTrack && sameArtist && sameAlbum && sameYear
 			}
 
+			// Returns if no data has been received
 			if (!data) return
 
-			const results = data.results
+			// Gets results
+			const { results } = data
 			let songData = undefined
 
 			let songId = 0
+			// If results contain array with more than one elements then formats proper display
 			if (results.length > 1) {
+				// Offset between last element and new column
 				const offset = 7
+				// Max number of width for track
 				let maxWidth_track = 50
+				// Max number of width for artist
 				let maxWidth_artist = 25
+				// Max number of width for album
 				let maxWidth_collection = 30
 
 				if (WINDOW_SCALING === 'auto') {
@@ -132,11 +169,14 @@ async function script() {
 					maxWidth_collection = Math.round(availableWidth * 0.25)
 				}
 
+				// Gets sorted and filtered results
 				const res = [...results].sort(sort).filter(filter)
 
+				// Adds new result for naming
 				res.unshift({})
 
 				const songs = res.reduce((prev, cur, id) => {
+					// Returns properly formatted columns naming
 					if (id === 0) {
 						return (prev +=
 							''.padEnd(5) +
@@ -150,17 +190,26 @@ async function script() {
 							'\n|  ')
 					}
 
+					// Gets track name  with limited length to `maxWidth_track`
 					const trackName = limitLength(cur.trackName, maxWidth_track)
+					// Gets artist with limited length to `maxWidth_artist`
 					const artistName = limitLength(cur.artistName, maxWidth_artist)
+					// Gets album name with limited length to `maxWidth_collection`
 					const collectionName = limitLength(cur.collectionName, maxWidth_collection)
+					// Gets year value
 					const year = (new Date(cur.releaseDate).getFullYear() || '').toString().padEnd(5)
 
+					// Properly formatted string
 					const song = trackName + '| ' + artistName + '| ' + year + '| ' + collectionName
+					// Properly formatted id
 					const songId = (id - 1).toString().padStart(3)
+					// Returns new string
 					return (prev += `${songId}: ${song}\n|  `)
 
 					function limitLength(text, maxWidth) {
+						// Checks whether text length is longer than `maxWidth`
 						const isLongerThanMax = text.length > maxWidth
+						// Adds '...' if `isLongerThanMax` is true
 						return text
 							.padEnd(maxWidth)
 							.substring(0, maxWidth)
@@ -169,6 +218,7 @@ async function script() {
 					}
 				}, '|\n|  ')
 
+				// Shows all songs
 				console.clear()
 				lineBreaker()
 				console.log(songs)
@@ -178,21 +228,26 @@ async function script() {
 
 				lineBreaker()
 			}
+			// Gets song data
 			songData = results[songId]
 
+			// Prints error if song data has not been found
 			console.clear()
 			if (songData === undefined) {
 				errorPrompt('Song not found')
 				return
 			}
 
+			// Returns object containing all data
 			const d = getData(songData)
 
+			// Shows proper data prompt
 			lineBreaker()
 			console.log(d.prompt)
 			copyrights()
 			lineBreaker()
 
+			// Opens lyrics and/or images if `open`, `openLyrics` or `openImage` property is set
 			if (d.data.lyrics && (properties.open || properties.openLyrics)) exec(`open ${d.data.lyrics}`)
 			if (d.data.artwork && (properties.open || properties.openImage)) exec(`open ${d.data.artwork}`)
 
@@ -203,7 +258,9 @@ async function script() {
 	function getData(song) {
 		const LYRICS_BASE_URL = 'https://www.azlyrics.com/lyrics/'
 		const date = new Date(song.releaseDate)
+		// Replaces 100x100 artwork format to desired resolution
 		const artwork = song.artworkUrl100?.replace('100x100bb.jpg', `${ARTWORK_SIZE}x${ARTWORK_SIZE}bb.jpg`)
+		// Returns properly formatted time of song
 		const time = {
 			minutes: Math.floor(song.trackTimeMillis / 1000 / 60)
 				.toString()
@@ -212,7 +269,9 @@ async function script() {
 				.toString()
 				.padStart(2, '0')
 		}
+		// Replaces everything inside of () with 'feat' or 'ft' inside
 		const replaceRegex = new RegExp(/\(*(ft|feat).*/, 'gi')
+		// Properly formats lyrics URL
 		const lyrics =
 			LYRICS_BASE_URL +
 			(
@@ -224,6 +283,7 @@ async function script() {
 				.trim()
 				.toLowerCase()
 
+		// Creates object with formatted data
 		const formattedData = {
 			name: song.trackName,
 			artistName: song.artistName.replaceAll(' & ', ', '),
@@ -237,6 +297,7 @@ async function script() {
 			disc: `${song.discNumber}/${song.discCount}`,
 			trackExplicitness: song.trackExplicitness
 		}
+		// Creates properly formatted prompt
 		const formattedPrompt = Object.keys(formattedData).reduce((acc, type) => {
 			return (acc += `${type}: ${formattedData[type]}\n|  `)
 		}, '|\n|  ')
@@ -244,8 +305,10 @@ async function script() {
 	}
 }
 async function downloadSong(url, song) {
+	// Gets file format
 	const format = properties.format || DEFAULT_AUDIO_FORMAT
 
+	// Gets whole file name
 	const fileName = `${song.artistName} - ${song.name}.${format}`
 	await getCommands(`yt-dlp -x -f ${format} --audio-quality 0 --add-metadata -o "${fileName}" ${url}`)
 
@@ -253,6 +316,7 @@ async function downloadSong(url, song) {
 	// Gets path to cover art
 	const coverArtPath = `./${coverArtName}.${ARTWORK_FORMAT}`
 
+	// Saves artwork to new file
 	await getCommands(`curl --output ${coverArtPath} ${song.artwork}`)
 
 	// Modifies metadata
