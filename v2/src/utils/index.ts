8b740db0f3b64105cc52c51edd51b95add186b60
Radosław Drab <radoslaw.drab03@gmail.com>
2024-05-11 11:13:19 +0200

    added trimText

--- v2/src/utils/index.ts
+++ v2/src/utils/index.ts
@@ -7,3 +7,10 @@ export function loopThroughKeys<Value extends any, T extends Record<string, Valu
 		return { key, value: obj[key] }
 	})
 }
+
+export function trimText(text: string, maxLength: number, minLength: number = 15) {
+	const maxGreatherThanMin = maxLength > minLength
+	return `${text.substring(0, maxGreatherThanMin ? maxLength - 3 : minLength)}${
+		text.length >= maxLength && text.length >= minLength ? '...' : ''
+	}`.padEnd(maxGreatherThanMin ? maxLength : minLength, ' ')
+}
