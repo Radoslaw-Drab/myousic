f20c20d8d0ff4d9e30a9456ed447c0d1e2bebee3
Radosław Drab <radoslaw.drab03@gmail.com>
2024-05-11 14:20:57 +0200

    added JSDoc

--- v2/src/utils/exec.ts
+++ v2/src/utils/exec.ts
@@ -1,5 +1,9 @@
 import { exec } from 'child_process'
 
+/**
+ * @param command Shell command
+ * @returns Shell output
+ */
 export async function getCommand(command: string) {
 	try {
 		return await new Promise((resolve: (value: string) => void, reject) => {
@@ -12,6 +16,19 @@ export async function getCommand(command: string) {
 		throw new Error(error)
 	}
 }
+/**
+ * @param commands Shell commands
+ * @returns Object with 3 keys: `settled`, `fullfilled`, `rejected`. Every key contains array of corresponding objects. Each object contains `value`, `cmd` (command) and `index` properties
+ * @example
+ * ```
+ * commands = ['echo test', 'test']
+ * {
+ * 	settled: [{status: 'fullfilled', value: 'test', cmd: 'echo test', index: 0}, { status: 'rejected', reason: '...', cmd: 'test', index: 1}]
+ * 	fullfilled: [{status: 'fullfilled', value: 'test', cmd: 'echo test', index: 0}]
+ * 	rejected: [{status: 'rejected', reason: '...', cmd: 'test', index: 1}]
+ * }
+ * ```
+ */
 export async function getCommands(...commands: string[]) {
 	const promises = commands.map((command) => getCommand(command))
 	const cmds = await Promise.allSettled(promises)
--- v2/src/utils/index.ts
+++ v2/src/utils/index.ts
@@ -1,14 +1,34 @@
+/**
+ * @description Loops throughout object keys
+ * @param {T} obj Custom object with string keys and custom values
+ * @param {function} callback Custom callback to loop throught object keys
+ * @returns {{key: string, value: any}[]} array of objects with key and value
+ */
 export function loopThroughKeys<Value extends any, T extends Record<string, Value> = {}>(
 	obj: T,
 	callback?: (key: keyof T, value: Value, index: number) => void
-) {
+): { key: string; value: Value }[] {
 	return Object.keys(obj).map((key, index) => {
 		callback && callback(key, obj[key], index)
 		return { key, value: obj[key] }
 	})
 }
 
-export function trimText(text: string, maxLength: number, minLength: number = 15) {
+/**
+ *
+ * @param {string} text text to be trimmed
+ * @param {number} maxLength max text length
+ * @param {number} minLength min text length
+ * @returns {string} trimmed text
+ * @example
+ * maxLength = 10
+ * minLength = 5
+ * `Testing code` --> `Testing...`
+ * `Test` --> `Test `
+ *
+ *
+ */
+export function trimText(text: string, maxLength: number, minLength: number = 15): string {
 	const maxGreatherThanMin = maxLength > minLength
 	return `${text.substring(0, maxGreatherThanMin ? maxLength - 3 : minLength)}${
 		text.length >= maxLength && text.length >= minLength ? '...' : ''
