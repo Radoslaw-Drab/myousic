d70785d6a37dfa1ed651a7c2b61d0b291f45252d
Radosław Drab <radoslaw.drab03@gmail.com>
2024-05-14 16:57:58 +0200

    added function to manipulate objects and arrays

--- v2/src/utils/index.ts
+++ v2/src/utils/index.ts
@@ -38,3 +38,36 @@ export function trim(text: string, maxLength: number): string {
 	const substring = text.substring(0, maxLength)
 	return `${substring.padEnd(maxLength, ' ')}`
 }
+
+export function createObjectFromArray<Key extends string, T extends string[] = []>(array: T, keys: Key[]): Record<Key, string> {
+	return array.reduce((obj, item, index) => {
+		return { ...obj, [keys[index] ?? index]: item }
+	}, {}) as Record<Key, string>
+}
+
+export function mapObjectsArrayToObject<T extends { key: string; value: any }>(array: T[], map?: (item: T, index: number) => T) {
+	const mappedArray = array.map((item, index) => (map ? map(item, index) : item))
+
+	return createObjectFromArray(
+		mappedArray.map((item) => item.value),
+		mappedArray.map((item) => item.key)
+	)
+}
+export function filterObjectValues<Value, T extends Record<string, Value> = {}>(
+	obj: T,
+	options: {
+		map?: (key: keyof T, value: Value, index: number) => { key: keyof T; value: Value }
+		beforeFilter?: (key: keyof T, value: Value, index: number) => boolean
+		afterFilter?: (key: keyof T, value: Value, index: number) => boolean
+	}
+) {
+	return (
+		loopThroughKeys<Value>(obj)
+			.filter(({ key, value }, index) => (options.beforeFilter ? options.beforeFilter(key, value, index) : true))
+			.map(({ key, value }, index) => options.map ? options.map(key, value, index): ({key, value}))
+			.filter(({ key, value }, index) => (options.afterFilter ? options.afterFilter(key, value, index) : true))
+			.reduce((obj, value) => {
+				return { ...obj, [value.key]: value.value }
+			}, {}) as Partial<T>
+	)
+}
