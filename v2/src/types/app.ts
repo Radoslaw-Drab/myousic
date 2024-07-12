c3f92019e5f3e8b0660be63981a17ea517d34697
Radosław Drab <radoslaw.drab03@gmail.com>
2024-07-12 20:51:00 +0200

    added app config

--- v2/src/types/app.ts
+++ v2/src/types/app.ts
@@ -1,6 +1,29 @@
+import { Config } from 'utils/class'
+
 import { Data } from './api'
 
 export type Modifier = 'a' | 'artist' | 't' | 'track' | 'al' | 'album' | 'y' | 'year'
 export type ModifiersObject = {
 	[key in Modifier]: keyof Data
 }
+
+export const settings: (keyof Settings)[] = [
+	'appName',
+	'saveFolder',
+	'artworkSize',
+	'artworkFormat',
+	'audioFormats',
+	'ytDlpPath'
+] as const
+export type SettingsKey = (typeof settings)[number]
+
+export type Settings = {
+	appName: string
+	saveFolder?: string
+	artworkSize?: number
+	artworkFormat?: 'jpg' | 'png'
+	ytDlpPath: string
+	audioFormats?: ('alac' | 'mp3' | 'm4a' | 'aac' | 'flac')[]
+}
+
+export type AppConfig = Config<Settings>
