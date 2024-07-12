4c00a9b1061f0970012bc5e0ac44986253b7876b
Radosław Drab <radoslaw.drab03@gmail.com>
2024-07-12 20:47:06 +0200

    added maybe partial type

--- v2/src/types/utils.ts
+++ v2/src/types/utils.ts
@@ -1,2 +1,3 @@
 export type Prefix<P extends string, C extends string> = `${P}${C}`
 export type Suffix<S extends string, C extends string> = `${C}${S}`
+export type MaybePartial<T> = T | Partial<T>
