98cdc8f468f7f75b986bd2b4163e8a72e8cefa14
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-10-18 19:45:41 +0200

    added sorting and fixed bugs

--- v3/utils/prompt.py
+++ v3/utils/prompt.py
@@ -1,5 +1,7 @@
 import keyboard
+from typing import Callable
 from utils.config import SortType
+from utils.classes import Listener
 from utils.colors import Color
 from utils.number import clamp
 from utils.system import clear 
@@ -9,17 +11,15 @@ cl = Color()
 class ListItem(dict):
   id: str
   name: str | None = None
+class ListSortFunction(Callable[[str, SortType], list[ListItem | str]]):
+  pass
 
 class List:
-  def __init__(self, items: list[ListItem | str], title: str | None = None, loop: bool = True, ordered: bool = True, multiple: bool = False, prefix: str | None = None, selector: str = '>', show_count: int = 10, before_screen: str | None = None, horizontal: bool = False):
-    self.items: list[ListItem | str] = []
+  def __init__(self, items: list[ListItem | str | None], title: str | None = None, loop: bool = True, ordered: bool = True, multiple: bool = False, prefix: str | None = None, selector: str = '>', show_count: int = 10, before_screen: str | None = None, horizontal: bool = False, sort_types: list[str] | None = None, sort_listener: ListSortFunction | None = None, show_info: bool = True):
+    self.items = self.__set_items(items)
     self.selected = []
-    self.__current_index = 0
-    for item in items:
-      if type(item) is str:
-        self.items.append({"id": item})
-      else:
-        self.items.append(item)
+    self.__current_index: int = 0
+
     self.title = title
     self.loop = loop
     self.ordered = ordered
@@ -29,7 +29,33 @@ class List:
     self.show_count = show_count
     self.before_screen = before_screen
     self.horizontal = horizontal
+    self.sort_types = sort_types
+    self.sort_type_index: int = -1
+    self.sort_dir: SortType = SortType.ASC
+    self.__allow_show_info = show_info
+    self.__show_info: bool = True
+    self.__set_ended(False)
+
+    self.__sort_listener = Listener() 
+
+    if sort_listener:
+      self.__sort_listener.set(sort_listener)
+  
+  def __repr__(self):
+    return self.get_index()
+  
+  def __set_items(self, items: list[ListItem | str], replace: bool = True):
+    new_items: list[ListItem] = [] if replace else self.items
+
+    for item in items:
+      if type(item) is str:
+        new_items.append({"id": item})
+      elif type(item) is dict:
+        new_items.append(item)
+    self.items = new_items
     
+    return new_items
+  
   def __up(self):
     if self.loop:
       self.__current_index -= 1
@@ -61,51 +87,53 @@ class List:
       self.selected = [i for i in range(len(self.items))]
     self.show()
       
+  def __toggle_show_info(self):
+    if self.__allow_show_info:
+      self.__show_info = not self.__show_info
+      self.show()
+  def __set_ended(self, value: bool):
+    self.__ended = value
   def get_index(self):
     self.show()
-    
+    keyboard.add_hotkey('enter', lambda: self.__set_ended(True))
     keyboard.add_hotkey('left' if self.horizontal else 'up', lambda: self.__up())
     keyboard.add_hotkey('right' if self.horizontal else 'down', lambda: self.__down())
 
     if self.multiple:
       keyboard.add_hotkey('space', lambda: self.__select_current())
       keyboard.add_hotkey('ctrl+a', lambda: self.select_all_toggle())
-
+    if self.sort_types != None and len(self.sort_types) > 0:
+      keyboard.add_hotkey('page up', lambda: self.__change_sort())
+      keyboard.add_hotkey('page down', lambda: self.__change_sort(-1))
+      keyboard.add_hotkey('tab', lambda: self.__change_sort_dir())
+    keyboard.add_hotkey('ctrl+i', lambda: self.__toggle_show_info())
     keyboard.wait('enter')
     clear()
     return self.__current_index
-  def get_value(self) -> str:
+  def get_value(self):
     index = self.get_index()
     return self.items[index].get('id')
   def show(self):
+    if self.__ended: 
+      return
     clear()
-    if self.before_screen != None:
-      print(self.before_screen)
-      if self.title != None:
-        print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
-
     show_count_half = round(self.show_count / 2)
     greater_than_show_count = len(self.items) >= self.show_count
-    items = list(filter(lambda i: i >= self.__current_index - show_count_half and i <= self.__current_index + show_count_half, range(len(self.items))))
-    reversed_items = self.items.copy()
-    reversed_items.reverse()
+    items = list(filter(lambda i: i >= self.__current_index - show_count_half and i < self.__current_index + show_count_half, range(len(self.items))))
 
     if len(items) < self.show_count and len(self.items) > self.show_count:
-      if items[len(items) - 1] >= len(self.items) - 1: 
+      is_last = items[len(items) - 1] >= len(self.items) - 1
+      if is_last: 
         for index in range(self.show_count - len(items)):
           items.append(index)
       else:
+        reversed_items = self.items.copy()
+        reversed_items.reverse()
         for index in range(len(reversed_items) - 1, len(reversed_items) - self.show_count + len(items) - 1, -1):
           items.insert(0, index)
+    self.show_info()
 
-    if self.multiple:
-      print(cl.change('(SPACE to select, CTRL+A for All)', cl.text.GREY))
-    if self.title != None:
-      if self.prefix != None:
-        print(cl.change(self.prefix, cl.text.BLUE), end=' ')
-      print(self.title)
-      print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
-
+    longestItemSize = max(*[len(item.get('name') or item.get('id')) for item in self.items])
     for index in items:
       item = self.items[index]
       value = item.get('name') or item.get('id')
@@ -115,11 +143,70 @@ class List:
       term = (f'{self.selector} ' if is_selected else '  ') + prefix + value if self.multiple else prefix + value
 
       end = '\n' if not self.horizontal or (self.horizontal and index == len(items) - 1) else ' | '
-      if index == 0 and greater_than_show_count and not self.horizontal:
-        print(cl.change('---', cl.text.GREY))
+      print_line = index == 0 and index != items[0] and greater_than_show_count and not self.horizontal
+      has_items_from_end = index < items[0]
+      is_last = index == items[len(items) - 1]
+      if print_line:
+        print(cl.change(''.ljust(longestItemSize + len(prefix), '-'), cl.text.GREY))
+
       if is_current_index:
         print(cl.change(term, cl.text.BLUE), end='')
       else:
         print(term, end='')
+      # print(is_first, is_last, end='')
       print(end, end='')
-    print()
+      if not has_items_from_end and is_last:
+        print()
+
+  def show_info(self):
+    if self.__allow_show_info:
+      print(cl.change('CTRL + I for info', cl.text.GREY))
+
+    if self.before_screen != None:
+      print(self.before_screen)
+      if self.title != None:
+        print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
+
+
+    if self.multiple and self.__show_info:
+      print(cl.change('(SPACE to select, CTRL+A for All)', cl.text.GREY))
+    
+
+    if self.title != None:
+      if self.prefix != None:
+        print(cl.change(self.prefix, cl.text.BLUE), end=' ')
+      print(self.title)
+      
+    if self.sort_types != None and len(self.sort_types) > 0:
+      print('Sort:', end=' ')
+      if self.sort_type_index != -1:
+        print(f'{cl.change(self.sort_types[self.sort_type_index], cl.options.URL)} ({self.sort_dir.value.upper()})', end=' ')
+      else:
+        print('-', end=' ')
+        
+      if self.__show_info:
+        print(cl.change('(Page Up/Down and TAB to change)', cl.text.GREY))
+      else:
+        print()
+
+    if self.title != None:
+      print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
+
+  def __change_sort(self, step: int = 1):
+    self.sort_type_index += max(min(step, 1), -1)
+    if self.sort_type_index >= len(self.sort_types):
+      self.sort_type_index = -1
+    if self.sort_type_index < -1:
+      self.sort_type_index = len(self.sort_types) - 1
+    self.__set_items(self.__sort_listener.emit(self.sort_types[self.sort_type_index], self.sort_dir))
+    self.show()
+  def __change_sort_dir(self):
+    if self.sort_type_index != -1:  
+      if self.sort_dir == SortType.ASC:
+        self.sort_dir = SortType.DESC
+      else:
+        self.sort_dir = SortType.ASC
+      self.__set_items(self.__sort_listener.emit(self.sort_types[self.sort_type_index], self.sort_dir))
+      self.show()
+  def set_sort_listener(self, listener: ListSortFunction | None):
+    self.__sort_listener.set(listener)
