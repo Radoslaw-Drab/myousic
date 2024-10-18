2668af74d74d721bf26bba7d78c977a3b779f75c
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-10-18 19:45:55 +0200

    added custom url

--- v3/track/track_data.py
+++ v3/track/track_data.py
@@ -8,12 +8,13 @@ from utils.text import uppercase
 class Lyrics:
   def __init__(self, lyrics_url: str = 'https://lyrist.vercel.app/api'):
     self.lyrics_url = lyrics_url
+    self.__custom_url: str | None = None
     pass
   def get_url(self, artist: str, title: str):
-    return (self.lyrics_url + '/' + urllib.parse.quote(artist + '/' + title)).lower()
+    return (self.lyrics_url + '/' + (urllib.parse.quote(artist + '/' + title)) if self.__custom_url == None else self.__custom_url).lower()
   def get(self, artist: str, title: str) -> str | None:
     try:
-      response = requests.get(self.get_url(artist, title))
+      response = requests.get(self.get_url(artist, title, self.__custom_url))
       return re.sub(r'\[.*\]\n', r'\r', re.sub(r'\n{2}', r'\r', response.json()['lyrics']))
     except:
       return None
