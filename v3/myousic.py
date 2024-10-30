8c2b0ea1260d45ee2d17847327b16f845cc11a6e
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-10-30 10:26:24 +0100

    updated to new prompts

--- v3/myousic.py
+++ v3/myousic.py
@@ -10,7 +10,8 @@ import re
 import requests
 
 from track import TrackExtended
-from utils.prompt import List, Input
+from utils import Exit
+from utils.prompt import List, Input, get_color, ColorType, print_color, print_formatted
 from utils.system import clear
 from utils.config import Config, SortType
 import utils.colors as colors
@@ -19,96 +20,106 @@ import utils.colors as colors
 cl = colors.Color()
 itunesApiUrl = 'https://itunes.apple.com/search'
 
-def main(*, url: str | None = None, search: str | None = None, download_only: bool = False):
-  clear()
+def youtube_dl():
   id = uuid4()
   options = {
     'format': 'm4a/bestaudio/best', 
     'outtmpl': f'{id}.%(ext)s',
     'quiet': 'true',
   }
+  return YoutubeDL(options)
+  
+def main(*, url: str | None = None, search: str | None = None, download_only: bool = False):
+  clear()
   config = Config(Path.home())
-
-  with YoutubeDL(options) as ydl:
-    if download_only and url:
+  ydl = youtube_dl()
+  if download_only and url:
+    try:
       track_download_only(ydl, id=id, url=url, config=config)
       end()
-      return
-    if search == None:
-      info = ydl.extract_info(url, download=False)
+    except Exit: 
+      pass
+    return
+  if search == None:
+    info = ydl.extract_info(url, download=False)
 
-      artist: str = info.get('artist') or info.get('uploader')
-      title: str = info.get('track') or info.get('fulltitle') or info.get('alt_title')
-
-
-      formattedTitle = re.sub(' x ', ', ', re.sub('(\\[|\\().*(\\]|\\))', '', title))
-
-      artist_match = re.match(r'.*(?= - )', formattedTitle)
-
-      if artist_match and info.get('artist') != None:
-        artist = artist_match.string
+    artist: str = info.get('artist') or info.get('uploader')
+    title: str = info.get('track') or info.get('fulltitle') or info.get('alt_title')
 
 
-      term = f'{artist} - {formattedTitle}' if re.search(artist, formattedTitle) == None else formattedTitle
-    else:
-      term = search
+    formattedTitle = re.sub(' x ', ', ', re.sub('(\\[|\\().*(\\]|\\))', '', title))
 
-    query = {
-      'term': term,
-      'limit': 200,
-      'entity': ['musicArtist', 'musicTrack', 'album', 'song']
-    }
-    q = urllib.parse.urlencode(query, doseq=True)
+    artist_match = re.match(r'.*(?= - )', formattedTitle)
 
-    response = requests.get(f'{itunesApiUrl}?{q}')
-    data: dict = response.json()
-    error = data.get('errorMessage')
-    if error:
-      print(cl.change(error, cl.text.RED))
-      input()
-      return
-    # results: list[dict] = data['results']
-    results: list[dict] = sorted(data['results'], key=lambda d: d[config.get_sort_key()], reverse=config.data.sort_type == SortType.DESC) if config.get_sort_key() != None else data['results']    
+    if artist_match and info.get('artist') != None:
+      artist = artist_match.string
 
 
-    if len(results) == 0:
-      return main(search=search_menu(cl.change(f'No results found for ', cl.text.RED) + cl.change(term, cl.text.BLUE)))
+    term = f'{artist} - {formattedTitle}' if re.search(artist, formattedTitle) == None else formattedTitle
+  else:
+    term = search
 
-    def maxSize(prop: str):
-      return max(0, *[len(result.get(prop) or '') for result in results]) if len(results) > 0 else 0
-    def get_date(date: str):
-      return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
-    maxArtistName = min(maxSize('artistName'), config.data.print_max_artist_size)
-    maxTrackName = min(maxSize('trackName'), config.data.print_max_track_size)
-    maxCollectionName = min(maxSize('collectionName'), config.data.print_max_album_size)
+  query = {
+    'term': term,
+    'limit': 200,
+    'entity': ['musicArtist', 'musicTrack', 'album', 'song']
+  }
+  q = urllib.parse.urlencode(query, doseq=True)
 
-    def get_result(dictionary: dict, prop: str, max_size: int):
-      return (dictionary.get(prop)[:max_size] if dictionary.get(prop) != None else '-').ljust(max_size)
+  response = requests.get(f'{itunesApiUrl}?{q}')
+  data: dict = response.json()
+  error = data.get('errorMessage')
+  if error:
+    print_color(error, ColorType.ERROR)
+    input()
+    return
+  results: list[dict] = sorted(data['results'], key=lambda d: d[config.get_sort_key()], reverse=config.data.sort_type == SortType.DESC) if config.get_sort_key() != None else data['results']    
 
-    def sort_results(sort_by: str | None, sort_type: SortType = SortType.ASC):
-      options: list[str] = []
-      sorted_results = sorted(results, key=lambda d: d[config.get_sort_key(sort_by)], reverse=sort_type == SortType.DESC) if sort_by != None else results
-      for r in sorted_results:
-        artistName = get_result(r, 'artistName', maxArtistName)
-        trackName = get_result(r, 'trackName', maxTrackName)
-        collectionName = get_result(r, 'collectionName', maxCollectionName)
-        year = get_date(r.get('releaseDate')).year if r.get('releaseDate') != None else '-'
-        options.append(f'{trackName} | {artistName} | {collectionName} | {year}')
-      return options
 
-    title = f"Select for {cl.change(term, cl.text.BLUE)}"
-    options = sort_results(None, config.data.sort_type)
+  if len(results) == 0:
+    return main(search=search_menu(get_color('No results found for ', ColorType.ERROR) + get_color(term, ColorType.PRIMARY)))
+
+  def maxSize(prop: str):
+    return max(0, *[len(result.get(prop) or '') for result in results]) if len(results) > 0 else 0
+  def get_date(date: str):
+    return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+  maxArtistName = min(maxSize('artistName'), config.data.print_max_artist_size)
+  maxTrackName = min(maxSize('trackName'), config.data.print_max_track_size)
+  maxCollectionName = min(maxSize('collectionName'), config.data.print_max_album_size)
+
+  def get_result(dictionary: dict, prop: str, max_size: int):
+    return (dictionary.get(prop)[:max_size] if dictionary.get(prop) != None else '-').ljust(max_size)
+
+  def sort_results(sort_by: str | None, sort_type: SortType = SortType.ASC):
+    options: list[str] = []
+    sorted_results = sorted(results, key=lambda d: d[config.get_sort_key(sort_by)], reverse=sort_type == SortType.DESC) if sort_by != None else results
+    for r in sorted_results:
+      artistName = get_result(r, 'artistName', maxArtistName)
+      trackName = get_result(r, 'trackName', maxTrackName)
+      collectionName = get_result(r, 'collectionName', maxCollectionName)
+      year = get_date(r.get('releaseDate')).year if r.get('releaseDate') != None else '-'
+      options.append(f'{trackName} | {artistName} | {collectionName} | {year}')
+    return options
+
+  title = f"Select for {get_color(term, ColorType.PRIMARY)}"
+  options = sort_results(None, config.data.sort_type)
+  try:
     index = List(options, title, sort_types=['title', 'artist', 'album', 'year'], sort_listener=sort_results, show_count=config.data.show_count).get_index() if len(options) > 1 else 0
     
-    
+    if index == None: return
     t = TrackExtended(results[index], id, config=config)
 
     if get_track(ydl, url, t):
       end()
+  except Exit:
+    return
 
 def end():
-  print(cl.change('Press enter to end', cl.text.GREY))
-  input()
+  try:
+    print_color('Press Enter to end', ColorType.GREY)
+    input()
+  except KeyboardInterrupt:
+    raise Exit
 def get_table(t: TrackExtended):
   t
   track = t.value
@@ -126,50 +137,53 @@ def get_table(t: TrackExtended):
     ['Track', f'{track.trackNumber} / {track.trackCount}' if track.trackNumber != None and track.trackCount != None else '-'],
     ['Disc', f'{track.discNumber} / {track.discCount}' if track.discNumber != None and track.discCount != None else '-'],
     SEPARATING_LINE,
-    ['Artwork', cl.change(t.get_artwork_url(), cl.options.URL) if t.get_artwork_url() else '-'],
-    ['Lyrics', cl.change(t.get_lyrics_url(), cl.options.URL)],
-    ['Genres', cl.change(t.get_genres_url(), cl.options.URL)]
+    ['Artwork', get_color(t.get_artwork_url(), ColorType.PRIMARY) if t.get_artwork_url() else '-'],
+    ['Lyrics', get_color(t.get_lyrics_url(), ColorType.PRIMARY)],
+    ['Genres', get_color(t.get_genres_url(), ColorType.PRIMARY)]
   ]
 
-  table = tabulate(data, tablefmt='simple')
+  table = tabulate(data, tablefmt='plain')
   return table
 def get_track(ydl: YoutubeDL, url: str | None, t: TrackExtended):
-
   valid_lyrics = t.check_lyrics()
   valid_genres = t.check_genres()
   table = get_table(t)
   before_screen = table
 
   if not valid_lyrics:
-    before_screen += cl.change('\nCouldn\'t find lyrics', cl.text.RED)
+    before_screen += f'\n{get_color('Couldn\'t find lyrics', ColorType.ERROR)}'
   if not valid_genres:
-    before_screen += cl.change('\nCouldn\'t find genres', cl.text.RED)
-
-  id = List([
-      { "id": "download", "name": "Download" } if url else None, 
-      { "id": "download-bare", "name": "Download without data" } if url else None, 
-      { "id": 'exit', "name": 'Exit' }
-    ], before_screen=before_screen, horizontal=True, show_info=False).get_value()
-  download = id == 'download' or id == 'download-bare'
-  get_lyrics = id != 'download-lyrics'
-  get_genres = id != 'download-genres'
+    before_screen += f'\n{get_color('Couldn\'t find genres', ColorType.ERROR)}'
   
-  if id == 'exit':
-    return False
-  
-  if url:
-    fileInfo = SimpleNamespace(**ydl.extract_info(url, download=download))
-    if download:
-      t.assign_file(fileInfo.audio_ext)
-    if download and id != 'download-bare':
-      t.metadata(get_lyrics=get_lyrics, get_genres=get_genres)
+  clear()
+  try:
+    id = List([
+        { "id": "download", "name": "Download" } if url else None, 
+        { "id": "download-bare", "name": "Download without data" } if url else None, 
+        { "id": 'exit', "name": 'Exit' }
+      ], before_screen=before_screen, horizontal=True).get_value()
+    download = id == 'download' or id == 'download-bare'
+    get_lyrics = id != 'download-lyrics'
+    get_genres = id != 'download-genres'
     
-
-  print(table)
-  if download:
-    print(cl.change('Downloaded', cl.text.GREEN))
-    t.save()
-  return True
+    if id == 'exit' or id == None:
+      return False
+    
+    if url:
+      fileInfo = SimpleNamespace(**ydl.extract_info(url, download=download))
+      if download:
+        t.assign_file(fileInfo.audio_ext)
+      if download and id != 'download-bare':
+        t.metadata(get_lyrics=get_lyrics, get_genres=get_genres)
+      
+    clear()
+    print_formatted(table)
+    if download:
+      print_color('Downloaded', ColorType.SUCCESS)
+      t.save()
+    return True
+  except Exit:
+    return False
 def track_download_only(ydl: YoutubeDL, *, id: str, url: str, config: Config):
   track = TrackExtended({}, id, config=config)
   
@@ -188,44 +202,56 @@ def track_download_only(ydl: YoutubeDL, *, id: str, url: str, config: Config):
 
   print(get_table(track))
   track.save()
-  
-  
+ 
 
 def search_menu(title: str | None = 'Search'):
-  [artist, title] = Input(title, 'Artist: ', 'Title: ').start()
-  return f'{artist} - {title}'
+  try:
+    [artist, title] = Input(title, 'Artist: ', 'Title: ').start()
+    return f'{artist} - {title}'
+  except Exit:
+    return None
 def valid_url(url: str | None):
   return bool(re.match(r'https?:\/\/(youtu\.be)|(youtube\.com)\/.*', url))
 def input_url(): 
-  clear()
-  url = pyperclip.paste()
-  print(cl.change(f'Clipboard URL: {url}' if valid_url(url) else 'No URL', cl.text.GREY))
-  url_input = input('YouTube URL' + ' (nothing for clipboard)' if valid_url(url) else '' + ':')
-  url = url_input if url_input != '' else url
-  if not valid_url(url):
-    return input_url()
+  try:
+    clear()
+    url = pyperclip.paste()
+    title = 'No URL'
+    if valid_url(url):
+      info = youtube_dl().extract_info(url, download=False)
+      title = (info.get('track') or info.get('fulltitle')) + ' / ' + (info.get('artist') or info.get('uploader'))
+    [url_input] = Input(get_color('Youtube info: ', ColorType.GREY) + get_color(title, ColorType.SECONDARY), 'YouTube URL' + (' (Enter for clipboard)' if valid_url(url) else '') + ': ').start()
+    url = url_input if url_input != '' else url
+    if not valid_url(url):
+      return input_url()
 
-  return url
+    return url
+  except Exit:
+    pass
+  return None
 def init():
   clear()
-  id = List([
-    {"id": "search-download", "name": "Search and Download"}, 
-    {"id": "search", "name": "Search"}, 
-    {"id": "download", "name": "Download"}, 
-    {"id": "exit", "name": "Exit"}
-    ], ordered=False, show_info=False, title=cl.change('  Myousic', cl.text.BLUE)).get_value()
-  if id == 'download' or id == 'search-download':
-    url = input_url()
-    # if not re.match(r'https?:\/\/(youtu\.be)|(youtube\.com)\/.*', url):
-    #   url = input('Youtube URL: ')
-    main(url=url, download_only= id == 'download')
-  elif id == 'search':
-    term = search_menu()
-    main(search=term)
-  elif id == 'exit':
+  try:
+    id = List([
+      {"id": "search-download", "name": "Search and Download"}, 
+      {"id": "search", "name": "Search"}, 
+      {"id": "download", "name": "Download"}, 
+      {"id": "exit", "name": "Exit"}
+      ], ordered=False, title=get_color('Myousic', ColorType.PRIMARY)).get_value()
+    if id == 'download' or id == 'search-download':
+      url = input_url()
+      if url != None:
+        main(url=url, download_only= id == 'download')
+    elif id == 'search':
+      term = search_menu()
+      if term != None:
+        main(search=term)
+    if id == 'exit' or id == None:
+      return
+    
+    init()
+  except Exit:
     return
-  
-  init()
   
 if __name__ == "__main__":
   init()
--- v3/utils/prompt.py
+++ v3/utils/prompt.py
@@ -1,5 +1,15 @@
-import keyboard
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.application import Application
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.layout import Layout, BufferControl, Window, HSplit, AnyContainer
+from prompt_toolkit.layout.processors import Processor, Transformation, TransformationInput
+from prompt_toolkit import PromptSession, print_formatted_text
+from prompt_toolkit.formatted_text import HTML, to_formatted_text, fragment_list_to_text
+from prompt_toolkit.widgets import Label, HorizontalLine
+from tabulate import tabulate
 from typing import Callable
+
+from utils import Exit
 from utils.config import SortType
 from utils.classes import Listener
 from utils.colors import Color
@@ -13,24 +23,37 @@ class Input:
     self.__prompts = prompts
     self.__values: list[str] = []
     self.__title = title
+    
+    self.__ps = PromptSession()
     pass
-  def start(self, clearScreen: bool = True):
-    if clearScreen:
+  def start(self, clear_screen: bool = True):
+    if clear_screen:
       clear()
     if self.__title:
-      print(self.__title)
-    for prompt in self.__prompts:
-      self.__values.append(input(prompt))
-    return self.__values
+      print_formatted_text(HTML(self.__title))
+    try:
+      for prompt in self.__prompts:
+        self.__values.append(self.__ps.prompt(HTML(prompt)))
+      return self.__values
+    except KeyboardInterrupt:
+      raise Exit
 
 class ListItem(dict):
   id: str
   name: str | None = None
+class ColorType():
+  PRIMARY = '#0000ff'
+  SECONDARY = '#ff5500'
+  GREY = '#555555'
+  ERROR = '#ff0000'
+  WARNING = '#ffff00'
+  SUCCESS = '#00ff00'
 class ListSortFunction(Callable[[str, SortType], list[ListItem | str]]):
   pass
 class List:
-  def __init__(self, items: list[ListItem | str | None], title: str | None = None, loop: bool = True, ordered: bool = True, multiple: bool = False, prefix: str | None = None, selector: str = '>', show_count: int = 10, before_screen: str | None = None, horizontal: bool = False, sort_types: list[str] | None = None, sort_listener: ListSortFunction | None = None, show_info: bool = True):
+  def __init__(self, items: list[ListItem | str | None], title: str | None = None, loop: bool = True, ordered: bool = True, multiple: bool = False, prefix: str | None = None, selector: str = '>', show_count: int = 10, before_screen: str | None = None, horizontal: bool = False, sort_types: list[str] | None = None, sort_listener: ListSortFunction | None = None, show_info: bool = False):
     self.items: list[ListItem] = self.__set_items(items)
+    self.__default_items = items
     self.selected = []
     self.__current_index: int = 0
 
@@ -46,19 +69,59 @@ class List:
     self.sort_types = sort_types
     self.sort_type_index: int = -1
     self.sort_dir: SortType = SortType.ASC
-    self.__allow_show_info = show_info
-    self.__show_info: bool = True
+    self.__show_info: bool = show_info
     self.__set_ended(False)
+    
+    self.__bindings = KeyBindings()
 
     self.__sort_listener = Listener() 
     
     if sort_listener:
       self.__sort_listener.set(sort_listener)
-  
-  def __repr__(self):
-    return self.get_index()
-  
-  def __set_items(self, items: list[ListItem | str | None], replace: bool = True):
+    
+    self.__buffer = Buffer()
+    self.__header = Label(self.title)
+    self.__controls = Label('')
+    self.__content = Window(content=BufferControl(buffer=self.__buffer, input_processors=[FormatText()]))
+    self.__update_root(HSplit([
+      self.__controls,
+      HorizontalLine(),
+      self.__header,
+      HorizontalLine(),
+      self.__content
+    ]))
+
+    self.__init_bindings()
+    
+  def __init_bindings(self):
+    @self.__bindings.add('enter')
+    def _(e): 
+      self.__set_ended(True)
+      e.app.exit(result=False)
+    self.__bindings.add('c-c')(lambda e: e.app.exit(result=True))
+    
+    self.__bindings.add('left' if self.horizontal else 'up')(lambda e: self.__up())
+    self.__bindings.add('right' if self.horizontal else 'down')(lambda e: self.__down())
+    if self.multiple:
+      self.__bindings.add('space')(lambda e: self.__select_current())
+      self.__bindings.add('c-a')(lambda e: self.select_all_toggle())
+    
+    if self.sort_types != None and len(self.sort_types) > 0:
+      self.__bindings.add('pageup')(lambda e: self.__change_sort())
+      self.__bindings.add('pagedown')(lambda e: self.__change_sort(-1))
+      self.__bindings.add('tab')(lambda e: self.__change_sort_dir())
+        
+    self.__bindings.add('c-i')(lambda e: self.__toggle_show_info())
+    # self.__bindings.add('s-tab')(lambda e: self.__toggle_show_info())
+  def __update_root(self, container: AnyContainer | Callable[[AnyContainer], AnyContainer]):
+    if isinstance(container, AnyContainer):
+      self.__root = container
+    else:
+      self.__root = container(self.__root)
+  def __set_items(self, items: list[ListItem | str | None] | None, replace: bool = True) -> list[ListItem]:
+    if items == None:
+      return self.__set_items(self.__default_items)
+
     new_items: list[ListItem] = [] if replace else self.items
 
     for item in items:
@@ -77,7 +140,7 @@ class List:
         self.__current_index = len(self.items) - 1
     else:
       self.__current_index = clamp(self.__current_index - 1, 0, len(self.items) - 1)
-    self.show()
+    self.__show()
   def __down(self):
     if self.loop:
       self.__current_index += 1
@@ -85,7 +148,7 @@ class List:
         self.__current_index = 0
     else:
       self.__current_index = clamp(self.__current_index + 1, 0, len(self.items) - 1)
-    self.show()
+    self.__show()
   def __select_current(self):
     self.__select(self.__current_index)
   def __select(self, index: int): 
@@ -93,44 +156,41 @@ class List:
       self.selected = list(filter(lambda i: i != index, self.selected))
     else:
       self.selected.append(index)
-    self.show()
+    self.__show()
   def select_all_toggle(self):
     if len(self.selected) == len(self.items):
       self.selected = []
     else:
       self.selected = [i for i in range(len(self.items))]
-    self.show()
+    self.__show()
       
   def __toggle_show_info(self):
-    if self.__allow_show_info:
-      self.__show_info = not self.__show_info
-      self.show()
+    self.__show_info = not self.__show_info
+    self.__show()
   def __set_ended(self, value: bool):
     self.__ended = value
   def get_index(self):
-    self.show()
-    keyboard.add_hotkey('enter', lambda: self.__set_ended(True))
-    keyboard.add_hotkey('left' if self.horizontal else 'up', lambda: self.__up())
-    keyboard.add_hotkey('right' if self.horizontal else 'down', lambda: self.__down())
+    try:
+      self.__show()
+      application = Application(Layout(self.__root), key_bindings=self.__bindings)
+      output = application.run()      
+      if output: raise Exit
 
-    if self.multiple:
-      keyboard.add_hotkey('space', lambda: self.__select_current())
-      keyboard.add_hotkey('ctrl+a', lambda: self.select_all_toggle())
-    if self.sort_types != None and len(self.sort_types) > 0:
-      keyboard.add_hotkey('page up', lambda: self.__change_sort())
-      keyboard.add_hotkey('page down', lambda: self.__change_sort(-1))
-      keyboard.add_hotkey('tab', lambda: self.__change_sort_dir())
-    keyboard.add_hotkey('ctrl+i', lambda: self.__toggle_show_info())
-    input()
-    clear()
-    return self.__current_index
+      return self.__current_index
+    except KeyboardInterrupt:
+      raise Exit
   def get_value(self):
     index = self.get_index()
     return self.items[index].get('id')
-  def show(self):
+  def __show(self):
     if self.__ended: 
-      return
-    clear()
+      return None
+    
+    self.__get_controls()
+    info = self.__get_info()
+    self.__header.formatted_text_control.text = HTML(info)
+    
+    text = ''
     show_count_half = round(self.show_count / 2)
     greater_than_show_count = len(self.items) >= self.show_count
     items = list(filter(lambda i: i >= self.__current_index - show_count_half and i < self.__current_index + show_count_half, range(len(self.items))))
@@ -145,7 +205,6 @@ class List:
         reversed_items.reverse()
         for index in range(len(reversed_items) - 1, len(reversed_items) - self.show_count + len(items) - 1, -1):
           items.insert(0, index)
-    self.show_info()
 
     longestItemSize = max(0, *[len(item.get('name') or item.get('id')) for item in self.items])
     for index in items:
@@ -161,50 +220,56 @@ class List:
       has_items_from_end = index < items[0]
       is_last = index == items[len(items) - 1]
       if print_line:
-        print(cl.change(''.ljust(longestItemSize + len(prefix), '-'), cl.text.GREY))
+        text += get_color(f'{(''.ljust(longestItemSize + len(prefix), '-'))}', ColorType.GREY) + '\n'
 
       if is_current_index:
-        print(cl.change(term, cl.text.BLUE), end='')
+        text += get_color(term, ColorType.PRIMARY)
       else:
-        print(term, end='')
-      # print(is_first, is_last, end='')
-      print(end, end='')
+        text += term
+      text += end
       if not has_items_from_end and is_last:
-        print()
-
-  def show_info(self):
-    if self.__allow_show_info:
-      print(cl.change('CTRL + I for info', cl.text.GREY))
-
-    if self.before_screen != None:
-      print(self.before_screen)
-      if self.title != None:
-        print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
-
-
-    if self.multiple and self.__show_info:
-      print(cl.change('(SPACE to select, CTRL+A for All)', cl.text.GREY))
+        text += '\n'
+    self.__buffer.reset()
+    self.__buffer.insert_text(text, True)
+  
+  def __get_controls(self):
+    def get(text: str):
+      return get_color(text, ColorType.GREY)
+    data: list[list[str]] = []
+    data.append([get('CTRL + I'), get('Show controls')])
     
+    if self.__show_info:
+      data.append([get('Left/Right Arrows' if self.horizontal else 'Up/Down Arrows'), get('Move left/right' if self.horizontal else 'Move up/down')])
+      data.append([get('CTRL + C'), get('Exit')])
+    
+      if self.sort_types != None and len(self.sort_types) > 0:
+        data.append([get('Page Up/Down'), get('Change type')])
+        data.append([get('TAB'), get('Change direction')])
+      
+      if self.multiple:
+        data.append([get('SPACE'), get('Select')])
+        data.append([get('CTRL + A'), get('Select all')])
+    
+    
+    table = tabulate(data, tablefmt='plain')
+    self.__controls.formatted_text_control.text = HTML(table)
+  def __get_info(self):
+    str = ''
+    if self.before_screen != None:
+      str += (self.before_screen) + '\n'
 
     if self.title != None:
       if self.prefix != None:
-        print(cl.change(self.prefix, cl.text.BLUE), end=' ')
-      print(self.title)
+        str += get_color(self.prefix, ColorType.SECONDARY) + ' '
+      str += (self.title)
       
     if self.sort_types != None and len(self.sort_types) > 0:
-      print('Sort:', end=' ')
+      str += '\nSort: '
       if self.sort_type_index != -1:
-        print(f'{cl.change(self.sort_types[self.sort_type_index], cl.options.URL)} ({self.sort_dir.value.upper()})', end=' ')
+        str += f'{self.sort_types[self.sort_type_index]} ({self.sort_dir.value.upper()})'
       else:
-        print('-', end=' ')
-        
-      if self.__show_info:
-        print(cl.change('(Page Up/Down and TAB to change)', cl.text.GREY))
-      else:
-        print()
-
-    if self.title != None:
-      print(cl.change(''.rjust(len(self.title), '-'), cl.text.GREY))
+        str += '- '
+    return str
 
   def __change_sort(self, step: int = 1):
     self.sort_type_index += max(min(step, 1), -1)
@@ -213,7 +278,7 @@ class List:
     if self.sort_type_index < -1:
       self.sort_type_index = len(self.sort_types) - 1
     self.__set_items(self.__sort_listener.emit(self.sort_types[self.sort_type_index], self.sort_dir))
-    self.show()
+    self.__show()
   def __change_sort_dir(self):
     if self.sort_type_index != -1:  
       if self.sort_dir == SortType.ASC:
@@ -221,6 +286,18 @@ class List:
       else:
         self.sort_dir = SortType.ASC
       self.__set_items(self.__sort_listener.emit(self.sort_types[self.sort_type_index], self.sort_dir))
-      self.show()
+      self.__show()
   def set_sort_listener(self, listener: ListSortFunction | None):
     self.__sort_listener.set(listener)
+
+def get_color(text: str, type: ColorType, modify_type: str = 'fg'):
+  return f'<style {modify_type}="{type}">{text}</style>'
+def print_formatted(text: str, sep: str = ' ', end: str = '\n'):
+  print_formatted_text(HTML(text), sep, end)
+def print_color(text: str, type: ColorType, modify_type: str = 'fg', sep: str = ' ', end: str = '\n'):
+  print_formatted(get_color(text, type, modify_type), sep=sep, end=end)
+
+class FormatText(Processor):
+    def apply_transformation(self, ti: TransformationInput):
+        fragments = to_formatted_text(HTML(fragment_list_to_text(ti.fragments)))
+        return Transformation(fragments)
