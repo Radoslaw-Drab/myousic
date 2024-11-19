2c9fdaf5e6857145b00911d994fb07fba7c36e50
Radosław Drab <radoslaw.drab03@gmail.com>
2024-11-19 06:54:59 +0100

    modified build file

--- v3/build.py
+++ v3/build.py
@@ -1,7 +1,25 @@
 import subprocess
+import argparse
+import sys
 
-requirements = subprocess.check_output(['pip', 'freeze'], text=True)
 
-open('requirements.txt', 'w').write(requirements)
+def get_args():
+  parser = argparse.ArgumentParser('build.py')
+  parser.add_argument('--build', '-b', help='Build script for your system', action='store_true')
+  parser.add_argument('--install', '-i', help='Install dependencies for script', action='store', metavar='ENV_NAME', type=str, default='dev')
+  return parser.parse_args()
 
-subprocess.run(['pyinstaller', '-F', 'myousic.py'])
+def main():
+  args = get_args()
+
+  if args.build:
+    requirements = subprocess.check_output(['pip', 'freeze'], text=True)
+    open('requirements.txt', 'w').write(requirements)
+    subprocess.run(['pyinstaller', '-F', 'myousic.py'])
+  if args.install:
+    subprocess.run([sys.executable,  '-m venv', args.install], shell=True)
+    subprocess.run(['sudo', 'dev/bin/activate'], shell=True)
+    subprocess.run(['pip',  'install',  '-r', 'requirements.txt'], shell=True)
+
+if __name__ == "__main__":
+  main()
