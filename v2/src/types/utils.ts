f84f9967c08b9e0cbae27f9b348bcd161525b093
Radosław Drab <radoslaw.drab03@gmail.com>
2024-11-22 23:01:01 +0100

    removed unnused version

--- v2/src/types/utils.ts
+++
@@ -1,3 +0,0 @@
-export type Prefix<P extends string, C extends string> = `${P}${C}`
-export type Suffix<S extends string, C extends string> = `${C}${S}`
-export type MaybePartial<T> = T | Partial<T>
--- v2/src/utils/app.ts
+++
@@ -1,9 +0,0 @@
-import constants from 'const'
-
-import { Track } from 'types/api'
-
-export function getLyrics(track: Track) {
-	const artist = track.artistName.toLowerCase().replace(/ /g, '')
-	const name = track.trackName.toLowerCase().replace(/ /g, '')
-	return constants.baseLyricsUrl + artist + '/' + name + '.html'
-}
